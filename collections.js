93bdafaa7934df99500e2e1ade821252ebb75741
Brylie Christopher Oxley <brylie@gnumedia.org>
2014-12-21 01:50:12 +0200

    Basic collections created.

--- collections.js
+++ collections.js
@@ -1,5 +1,23 @@
+// Housing accommodations during the event
 Accommodations = new Meteor.Collection("accommodations");
-
 Accommodations.attachSchema(new SimpleSchema({
+
+}));
+
+// Registrant age groups, used for pricing calculations, etc.
+AgeGroups = new Meteor.Collection("ageGroups");
+AgeGroups.attachSchema(new SimpleSchema({
+
+}));
+
+// Individual registrant details
+Registrants = new Meteor.Collection("registrants");
+Registrants.attachSchema(new SimpleSchema({
+
+}));
+
+// Event registrations, consisting of one or more registrants
+Registrations = new Meteor.Collection("registrations");
+Registrations.attachSchema(new SimpleSchema({
 
 }));
