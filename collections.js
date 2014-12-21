5701c305554d7566e1fb94a39938898923e4e44b
Brylie Christopher Oxley <brylie@gnumedia.org>
2014-12-21 04:26:09 +0200

    Added initialization data.

--- collections.js
+++ collections.js
@@ -1,23 +1,38 @@
 // Housing accommodations during the event
-Accommodations = new Meteor.Collection("accommodations");
-Accommodations.attachSchema(new SimpleSchema({
+Accommodations = new Meteor.Collection("Accommodations");
+//Accommodations.attachSchema(new SimpleSchema({
+//    title: {
+//        type: String,
+//        label: "Title",
+//        max: 200
+//    }
+//}));
+//
+//// Registrant age groups, used for pricing calculations, etc.
+AgeGroups = new Meteor.Collection("Age Groups");
+//AgeGroups.attachSchema(new SimpleSchema({
+//    title: {
+//        type: String,
+//        label: "Title",
+//        max: 200
+//    }
+//}));
 
-}));
-
-// Registrant age groups, used for pricing calculations, etc.
-AgeGroups = new Meteor.Collection("ageGroups");
-AgeGroups.attachSchema(new SimpleSchema({
-
-}));
-
-// Individual registrant details
-Registrants = new Meteor.Collection("registrants");
-Registrants.attachSchema(new SimpleSchema({
-
-}));
-
-// Event registrations, consisting of one or more registrants
-Registrations = new Meteor.Collection("registrations");
-Registrations.attachSchema(new SimpleSchema({
-
-}));
+//// Individual registrant details
+//Registrants = new Meteor.Collection("registrants");
+//Registrants.attachSchema(new SimpleSchema({
+//    first_name: {
+//        type: String,
+//        label: "First Name",
+//        max: 200
+//    }
+//}));
+//
+//// Event registrations, consisting of one or more registrants
+//Registrations = new Meteor.Collection("registrations");
+//Registrations.attachSchema(new SimpleSchema({
+//    registrants: {
+//        type: [Registrants],
+//        optional: false
+//    }
+//}));
