a89ba55de971d26ad66222ddc59dd6cfc6513fd1
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-01 14:57:38 +0200

    Renamed file/template helper.

--- client/templates/register/edit.js
+++ client/templates/register/edit.js
@@ -1,8 +1,4 @@
-// Subscribe to registrations.
-// TODO: Filter to only authorized registrations.
-Meteor.subscribe('registrations');
-
-Template.registration.helpers({
+Template.editRegistration.helpers({
     // Get the current registration from the URL path
     getRegistration: function () {
         var registrationId = Router.current().params._id;
