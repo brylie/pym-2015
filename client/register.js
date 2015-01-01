c44f08418c0ce48be22fa2ee3d8e2be5f101f3d7
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-01 13:35:27 +0200

    Fixed strange field behavior.

--- client/register.js
+++ client/register.js
@@ -7,5 +7,21 @@ Template.register.helpers({
             {label: "Young Friend (18-25)", value: "youngAdult"},
             {label: "Adult (26+)", value: "adult"}
         ];
+    },
+    registrationTypeoptions: function () {
+        return [
+            {label: "Commuter", value: "commuter"},
+            {label: "Daily", value: "daily"},
+            {label: "Full Week", value: "weekly"}
+        ];
+    },
+    accommodationsOptions: function () {
+        return [
+            {label: "Child (0-5)", value: "child"},
+            {label: "Youth (6-12)", value: "youth"},
+            {label: "Teen (13-17)", value: "teen"},
+            {label: "Young Friend (18-25)", value: "youngAdult"},
+            {label: "Adult (26+)", value: "adult"}
+        ];
     }
 });
