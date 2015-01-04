fa84e4def735f4dad0521f4053ead3944f04cb84
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-04 16:32:54 +0200

    Placeholder for price calculation.

--- client/templates/register/register.js
+++ client/templates/register/register.js
@@ -10,3 +10,9 @@ logValues = function () {
     console.log("Age group: " + ageGroup);
     console.log("Registration type: " + registrationType);
 }
+
+calculatePrice = function (ageGroup, registrationType, days) {
+    // calculate the registration price based on age group, registration type
+    // if applicable, based on days attending
+    // also add extras such as linens, etc.
+}
