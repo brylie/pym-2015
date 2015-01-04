304e80863670be5aaff4ad4607a493b1278cbfb4
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-04 19:42:12 +0200

    Clean-up.

--- client/templates/register/registrant.js
+++ client/templates/register/registrant.js
@@ -1,5 +1,5 @@
 Template.registrantDetails.helpers({
-    options: function () {
+    ageGroupOptions: function () {
         return [
             {label: "Child (0-5)", value: "child"},
             {label: "Youth (6-12)", value: "youth"},
@@ -13,15 +13,6 @@ Template.registrantDetails.helpers({
             {label: "Commuter", value: "commuter"},
             {label: "Daily", value: "daily"},
             {label: "Full Week", value: "weekly"}
-        ];
-    },
-    accommodationsOptions: function () {
-        return [
-            {label: "Child (0-5)", value: "child"},
-            {label: "Youth (6-12)", value: "youth"},
-            {label: "Teen (13-17)", value: "teen"},
-            {label: "Young Friend (18-25)", value: "youngAdult"},
-            {label: "Adult (26+)", value: "adult"}
         ];
     }
 });
