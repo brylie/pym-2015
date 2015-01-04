eb7f4304b2dcfda139e2e462011a34766c41ce44
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-04 19:41:17 +0200

    Added accommodations options selection.

---
+++ client/templates/register/accommodations.js
@@ -0,0 +1,11 @@
+Template.accommodations.helpers({
+    'accommodationsOptions': function () {
+        return [
+            {label: "Camping", value: "camping"},
+            {label: "Semi-private", value: "semi-private"},
+            {label: "Private", value: "private"},
+            {label: "Junior Yearly Meeting", value: "jym"},
+            {label: "Young Adult Friends", value: "yaf"}
+        ];
+    }
+});
