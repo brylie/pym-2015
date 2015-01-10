34059070c33962517665f91061f5005f250408a3
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-10 13:29:10 +0200

    Added accommodations options logic for age group.

---
+++ client/templates/register/accommodations_options.js
@@ -0,0 +1,43 @@
+Template.accommodationsOptions.helpers({
+    'adultAccommodationsOptions': function () {
+        return [
+            {label: "Camping", value: "camping"},
+            {label: "Semi-private", value: "semi-private"},
+            {label: "Private", value: "private"},
+            {label: "Young Adult Friends", value: "yaf"}
+        ];
+    },
+   'youngAdultAccommodationsOptions': function () {
+        return [
+            {label: "Camping", value: "camping"},
+            {label: "Semi-private", value: "semi-private"},
+            {label: "Private", value: "private"},
+            {label: "Junior Yearly Meeting", value: "jym"},
+            {label: "Young Adult Friends", value: "yaf"}
+        ];
+    },
+    'teenAccommodationsOptions': function () {
+        return [
+            {label: "Camping", value: "camping"},
+            {label: "Semi-private", value: "semi-private"},
+            {label: "Private", value: "private"},
+            {label: "Junior Yearly Meeting", value: "jym"},
+            {label: "Young Adult Friends", value: "yaf"}
+        ];
+    },
+    'youthAccommodationsOptions': function () {
+        return [
+            {label: "Camping", value: "camping"},
+            {label: "Semi-private", value: "semi-private"},
+            {label: "Private", value: "private"},
+            {label: "Junior Yearly Meeting", value: "jym"}
+        ];
+    },
+    'childAccommodationsOptions': function () {
+        return [
+            {label: "Camping", value: "camping"},
+            {label: "Semi-private", value: "semi-private"},
+            {label: "Private", value: "private"}
+        ];
+    },
+});
