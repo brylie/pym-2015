908b8f5182e4421be0b6b585c3aaf4ae4b6536d4
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-11 15:50:43 +0200

    Fixed typo.  Select elements reset on render.

--- client/templates/register/register.js
+++ client/templates/register/register.js
@@ -3,12 +3,21 @@ Template.register.created = function () {
     // These variables may be used in dynamic calculations
     ageGroupVar = new ReactiveVar;
     registrationTypeVar = new ReactiveVar;
-    accommodationsVar = new ReactiveVar
+    accommodationsVar = new ReactiveVar;
 };
+
+Template.register.rendered = function () {
+    // Reset form select field values
+    // to prevent an edge case bug for code push
+    // where accommodations value was undefined
+    $('#age_group').val('');
+    $('#registration_type').val('');
+    $('#accommodations').val('');
+}
 
 Template.register.helpers({
     'price': function () {
-        return calculateRegistrationPrice();
+        return calculateRegistrationPrice(ageGroupVar, registrationTypeVar, accommodationsVar);
     }
 });
 
@@ -17,11 +26,11 @@ Template.register.events({
         // Set the age group reactive variable
         ageGroupVar.set($('#age_group').val());
     },
-     'change #registration_type': function () {
+    'change #registration_type': function () {
         // Set the registration type reactive variable
         registrationTypeVar.set($('#registration_type').val());
     },
-     'change #accommodations': function () {
+    'change #accommodations': function () {
         // Set the accommodations reactive variable
         accommodationsVar.set($('#accommodations').val());
     }
