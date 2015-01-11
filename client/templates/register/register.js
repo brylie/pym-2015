0dd83a8e7c0693fd3081d13c92dcb65c7e7ec92b
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-11 14:22:36 +0200

    Added reactive variables for price calculation.

---
+++ client/templates/register/register.js
@@ -0,0 +1,7 @@
+Template.register.created = function () {
+    // Set up reactive variables to temporarily hold the values of registration form fields
+    // These variables may be used in dynamic calculations
+    ageGroupVar = new ReactiveVar;
+    registrationTypeVar = new ReactiveVar;
+    accommodationsVar = new ReactiveVar
+};
