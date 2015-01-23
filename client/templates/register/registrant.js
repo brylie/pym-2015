a4150ebb6c180f080572ad24432781e1adc8697d
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-23 09:33:45 +0200

    Removed ageGroupOptions. Cleanup.

--- client/templates/register/registrant.js
+++ client/templates/register/registrant.js
@@ -1,14 +1,6 @@
 Template.registrantDetails.helpers({
-    ageGroupOptions: function () {
-        return [
-            {label: "Child (0-5)", value: "child"},
-            {label: "Youth (6-12)", value: "youth"},
-            {label: "Teen (13-17)", value: "teen"},
-            {label: "Young Friend (18-25)", value: "youngAdult"},
-            {label: "Adult (26+)", value: "adult"}
-        ];
-    },
-    registrationTypeOptions: function () {
+    'registrationTypeOptions': function () {
+        // registration types used on the registration form
         return [
             {label: "Commuter", value: "commuter"},
             {label: "Daily", value: "daily"},
@@ -19,23 +11,34 @@ Template.registrantDetails.helpers({
 
 Template.registrantDetails.events({
     'change #registration_type': function () {
-        // trying to reset days selection when template changes
-        // for some reason this is not working, or is it?
+        // clean up form values for accommodations and days
+        // to make sure no erroneous values remain
+
         resetReactiveVars();
+
+        // Clear the accommodations selection
         $("#accommodations").val("");
 
-        // cycle through each day
+        // Clear all day checkboxes
         $('input[name="days"]').each(function() {
             // make sure day is not checked
             this.checked = false;
         });
     },
-    'change #age_group': function () {
-        console.log("Age group changed.");
+    'change #age': function () {
+        // Get the value of the age field
+        var ageValue = $('#age').val();
+
+        // Calculate the age group based on the age value
+        var ageGroup = calculateAgeGroup(ageValue);
+
+        // Reset the accommodations selection
+        // as accommodations depend on age group
         $("#accommodations").val("");
-//        registrationFeeVar.set();
-//        resetReactiveVars();
-//        setReactiveVars();
-//        setRegistrationFeeVar();
+
+        // Set the age group reactive variable
+        // for price calculations
+        ageGroupVar.set(ageGroup);
+        console.log("Age group calculation: " + ageGroupVar.get());
     }
 });
