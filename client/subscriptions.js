b4e44af7c37cf21d686487c03c3575c5c2d2b358
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-26 00:35:41 +0200

    Added ageGroup pubsub

--- client/subscriptions.js
+++ client/subscriptions.js
@@ -1,3 +1,8 @@
 // Subscribe to registrations.
 // TODO: Filter to only authorized registrations.
 Meteor.subscribe('registrants');
+Meteor.subscribe('childRegistrantCount');
+Meteor.subscribe('youthRegistrantCount');
+Meteor.subscribe('teenRegistrantCount');
+Meteor.subscribe('youngAdultRegistrantCount');
+Meteor.subscribe('adultRegistrantCount');
