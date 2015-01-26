249ce03d8e02f9a4b60b3b9c53a92530d773281c
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-01-26 10:12:49 +0200

    Moved.

--- client/subscriptions.js
+++
@@ -1,8 +0,0 @@
-// Subscribe to registrations.
-// TODO: Filter to only authorized registrations.
-Meteor.subscribe('registrants');
-Meteor.subscribe('childRegistrantCount');
-Meteor.subscribe('youthRegistrantCount');
-Meteor.subscribe('teenRegistrantCount');
-Meteor.subscribe('youngAdultRegistrantCount');
-Meteor.subscribe('adultRegistrantCount');
