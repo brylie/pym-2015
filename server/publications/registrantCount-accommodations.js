14d429b53f4770a2eb06a04af5d182269bc29f23
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-02-08 20:52:35 +0200

    Removed Private. Added Dorm Proximate and Distant.

--- server/publications/registrantCount-accommodations.js
+++ server/publications/registrantCount-accommodations.js
@@ -1,16 +1,40 @@
+/*
+* These publications provide a count of various Accommodations options chosen by registrants.
+*/
+
 // Accommodations count publications
 Meteor.publish('campingRegistrantAccommodationCount', function() {
-    Counts.publish(this, 'campingRegistrantAccommodationCount', Registrants.find({'accommodations': 'camping'}));
+    Counts.publish(this,
+                   'campingRegistrantAccommodationCount',
+                   Registrants.find({'accommodations': 'camping'}));
 });
+
+Meteor.publish('dormDistantRegistrantAccommodationCount', function() {
+    Counts.publish(this,
+                   'dormDistantRegistrantAccommodationCount',
+                   Registrants.find({'accommodations': 'dorm-distant-bathroom'}));
+});
+
+Meteor.publish('dormProximateRegistrantAccommodationCount', function() {
+    Counts.publish(this,
+                   'dormProximateRegistrantAccommodationCount',
+                   Registrants.find({"accommodations": "dorm-proximate-bathroom"}));
+});
+
 Meteor.publish('semiprivateRegistrantAccommodationCount', function() {
-    Counts.publish(this, 'semiprivateRegistrantAccommodationCount', Registrants.find({'accommodations': 'semiprivate'}));
+    Counts.publish(this,
+                   'semiprivateRegistrantAccommodationCount',
+                   Registrants.find({'accommodations': 'semiprivate'}));
 });
-Meteor.publish('privateRegistrantAccommodationCount', function() {
-    Counts.publish(this, 'privateRegistrantAccommodationCount', Registrants.find({'accommodations': 'private'}));
-});
+
 Meteor.publish('jymRegistrantAccommodationCount', function() {
-    Counts.publish(this, 'jymRegistrantAccommodationCount', Registrants.find({'accommodations': 'jym'}));
+    Counts.publish(this,
+                   'jymRegistrantAccommodationCount',
+                   Registrants.find({'accommodations': 'jym'}));
 });
+
 Meteor.publish('yafRegistrantAccommodationCount', function() {
-    Counts.publish(this, 'yafRegistrantAccommodationCount', Registrants.find({'accommodations': 'yaf'}));
+    Counts.publish(this,
+                   'yafRegistrantAccommodationCount',
+                   Registrants.find({'accommodations': 'yaf'}));
 });
