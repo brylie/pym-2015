ae5b8b0a3de5f8c1658bbf1bc20fed3831c5fe2b
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-02-08 20:51:12 +0200

    Added dorm subscriptions.

--- client/subscriptions/countAccommodations.js
+++ client/subscriptions/countAccommodations.js
@@ -1,6 +1,7 @@
 // Subscribe to accommodation counts
 Meteor.subscribe('campingRegistrantAccommodationCount');
 Meteor.subscribe('semiprivateRegistrantAccommodationCount');
-Meteor.subscribe('privateRegistrantAccommodationCount');
+Meteor.subscribe('dormDistantRegistrantAccommodationCount');
+Meteor.subscribe('dormProximateRegistrantAccommodationCount');
 Meteor.subscribe('yafRegistrantAccommodationCount');
 Meteor.subscribe('jymRegistrantAccommodationCount');
