93c9e00f5b16142f8e9759b7397248c8ecbf1ff4
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-03-22 19:15:43 +0200

    Added route authorization functions.

--- both/router/routes.js
+++ both/router/routes.js
@@ -1,8 +1,10 @@
 Router.route('/', {
-  name: 'home'
+    name: 'home'
 });
 
-Router.route('/dashboard');
+Router.route('/dashboard', {
+
+});
 
 //Router.route('/', function () {
 //    //this.render('welcome', {data: {title: '2015 PYM Registration'}});
@@ -15,3 +17,29 @@ Router.route('/register');
 //Router.route('/registration/:_id', function () {
 //    this.render('registration');
 //});
+
+var requiresUserLogin = function () {
+    if (!Meteor.user()) {
+        this.render('login');
+    } else {
+        this.next();
+    }
+};
+
+// Make sure user is logged in with registrar role
+var requiresRegistrarAccess = function () {
+    if (!Meteor.user() ||
+        !Meteor.user().roles ||
+        !Meteor.user().roles.registrar)
+    {
+        this.render('notFound');
+    } else {
+        this.next();
+    }
+};
+
+// User login required for event registration
+Router.onBeforeAction(requiresUserLogin, {only: ['register']});
+
+// Admin-only access to certain routes
+Router.onBeforeAction(requiresRegistrarAccess, {only: ['dashboard']});
