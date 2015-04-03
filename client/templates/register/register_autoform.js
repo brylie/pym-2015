ade7e2455d09fab217f7c8620bc93def106c1e26
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-04-03 18:12:55 +0300

    Flash message on success.

--- client/templates/register/register_autoform.js
+++ client/templates/register/register_autoform.js
@@ -2,5 +2,8 @@ AutoForm.addHooks('registration-form', {
     onSuccess: function(operation, result, template) {
         // Make sure all reactive vars are up to date.
         resetReactiveVars();
+
+        // Tell the user the registration was successful
+        FlashMessages.sendSuccess("Success! Registration submitted.");
     }
 });
