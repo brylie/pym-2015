f59326dfb92278192d894ff58da6189dba550de5
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-04-05 17:18:40 +0300

    Switched to subscriptions and action.

--- both/controllers/view.js
+++ both/controllers/view.js
@@ -1,8 +1,18 @@
 ViewController = AppController.extend({
-    waitOn: function() {
+    subscriptions: function() {
+        // Subscribe to user registrations
         return this.subscribe('myRegistrants');
     },
-    data: {
-
+    action: function () {
+        // Make sure subscriptions are available before rendering
+        // Otherwise, show loading template
+        if (this.ready()) {
+            this.render();
+        } else {
+            this.render('loading');
+        }
+    },
+    data: function () {
+        return Registrants.find();
     }
 });
