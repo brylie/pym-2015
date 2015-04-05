8ac901b9c2f28f795eb6376e6aa976f629231986
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-04-05 20:53:13 +0300

    Check if SMTP is enabled.

--- server/methods/email.js
+++ server/methods/email.js
@@ -1,16 +1,28 @@
 Registrants.after.insert(function (userId, document) {
-    var to = document.createdByEmail,
-        from = process.env.MAIL_FROM,
-        name = document.first_name,
-        html = 'We have received your event registration for PYM 2015. <br /> \
-                        View your registration(s) by visiting: <br /> \
-                        <a href="http://register.pacificyearlymeeting.org/view">\
-                        http://register.pacificyearlymeeting.org/view</a>';
+    // If SMTP is configured, send email when registration is added
+    // Check whether email sending is enabled
+    var emailEnabled = process.env.ENABLE_SMTP;
 
-    Email.send({
-        from: from,
-        to: to,
-        subject: "[PYM 2015] Registration success!",
-        html: html
-    });
+    if (emailEnabled) {
+        // Email settings
+
+        // Email address from registration
+        var to = document.createdByEmail;
+
+        // From address from mail configuration
+        var from = process.env.MAIL_FROM;
+
+        // HTML message
+        var html = 'We have received your event registration for PYM 2015. <br /> \
+                                View your registration(s) by visiting: <br />\
+                                <a href="http://register.pacificyearlymeeting.org/view">\
+                                http://register.pacificyearlymeeting.org/view</a>';
+
+        Email.send({
+            from: from,
+            to: to,
+            subject: "[PYM 2015] Registration success!",
+            html: html
+        });
+    }
 });
