6bd962e9a526eba9ce872475521140468141ef57
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-04-05 20:16:12 +0300

    HTML message. Cleanup.

--- server/methods/email.js
+++ server/methods/email.js
@@ -1,12 +1,16 @@
-Registrants.after.insert(function (userId, document){
+Registrants.after.insert(function (userId, document) {
     var to = document.createdByEmail,
         from = process.env.MAIL_FROM,
-        name = document.first_name;
+        name = document.first_name,
+        html = 'We have received your event registration for PYM 2015. <br /> \
+                        View your registration(s) by visiting: <br /> \
+                        <a href="http://register.pacificyearlymeeting.org/view">\
+                        http://register.pacificyearlymeeting.org/view</a>';
 
     Email.send({
         from: from,
         to: to,
-        subject: "Put you subject here " + name,
-        text: "Here is some text"
+        subject: "[PYM 2015] Registration success!",
+        html: html
     });
 });
