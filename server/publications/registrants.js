fe08484af2d3c25f7e2c59da836ede57a4afe921
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-04-05 16:52:37 +0300

    Added myRegistrants publication.

--- server/publications/registrants.js
+++ server/publications/registrants.js
@@ -2,3 +2,10 @@ Meteor.publish('registrants', function () {
     return Registrants.find();
 });
 
+Meteor.publish('myRegistrants', function () {
+    // Check if the user is signed in
+    if (this.userId) {
+        // return registrations for this user
+        return Registrants.find({'registeredById': this.userId});
+    }
+});
