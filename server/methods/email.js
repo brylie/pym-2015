60111ddbcd806eef8eb1f21f861ccb75ae4fa264
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-04-11 13:03:44 +0300

    Changed collection hook to Meteor method.

--- server/methods/email.js
+++ server/methods/email.js
@@ -1,28 +1,37 @@
-Registrants.after.insert(function (userId, document) {
-    // If SMTP is configured, send email when registration is added
-    // Check whether email sending is enabled
-    var emailEnabled = process.env.ENABLE_SMTP;
+Meteor.methods({
+    /*
+    Send a confirmation email based on
+        - registrant ID
+    */
+    'sendConfirmationEmail': function (registrantId) {
+        // Get the registration document
+        var registration = Registrants.findOne(registrantId);
 
-    if (emailEnabled) {
-        // Email settings
+        // If SMTP is configured, send email when registration is added
+        // Check whether email sending is enabled
+        var emailEnabled = process.env.ENABLE_SMTP;
 
-        // Email address from registration
-        var to = document.createdByEmail;
+        if (emailEnabled) {
+            // Set up email message and send
 
-        // From address from mail configuration
-        var from = process.env.MAIL_FROM;
+            // Email address from registration
+            var to = registration.createdByEmail;
 
-        // HTML message
-        var html = 'We have received your event registration for PYM 2015. <br /> \
-                                View your registration(s) by visiting: <br />\
-                                <a href="http://register.pacificyearlymeeting.org/view">\
-                                http://register.pacificyearlymeeting.org/view</a>';
+            // From address from mail configuration
+            var from = process.env.MAIL_FROM;
 
-        Email.send({
-            from: from,
-            to: to,
-            subject: "[PYM 2015] Registration success!",
-            html: html
-        });
+            // HTML message
+            var html = 'We have received your event registration for PYM 2015. <br /> \
+View your registration(s) by visiting: <br />\
+<a href="http://register.pacificyearlymeeting.org/view">\
+http://register.pacificyearlymeeting.org/view</a>';
+
+            Email.send({
+                from: from,
+                to: to,
+                subject: "[PYM 2015] Registration success!",
+                html: html
+            });
+        }
     }
 });
