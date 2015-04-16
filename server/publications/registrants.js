84aecdf384d0be39c38d5c80100eb3725c0fb74c
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-04-16 15:21:15 +0300

    Added 'my registrants' count. Comments.

--- server/publications/registrants.js
+++ server/publications/registrants.js
@@ -2,10 +2,16 @@ Meteor.publish('registrants', function () {
     return Registrants.find();
 });
 
+// Registrations for a given user
 Meteor.publish('myRegistrants', function () {
     // Check if the user is signed in
     if (this.userId) {
         // return registrations created by this user
         return Registrants.find({'createdById': this.userId});
     }
+});
+
+// Count of registrations for a given user
+Meteor.publish('myRegistrantCount', function() {
+    Counts.publish(this, 'myRegistrantCount', Registrants.find({'createdById': this.userId}));
 });
