d3d987050935620f88743bfa3708e351bd3f1343
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-04-21 05:39:20 +0300

    Address in email confirmation.

--- server/methods/email.js
+++ server/methods/email.js
@@ -21,10 +21,14 @@ Meteor.methods({
             var from = process.env.MAIL_FROM;
 
             // HTML message
-            var html = 'We have received your event registration for PYM 2015. <br /> \
+            var html = '<p>We have received your event registration for PYM 2015. <br /> \
 View your registration(s) by visiting: <br />\
 <a href="http://register.pacificyearlymeeting.org/view">\
-http://register.pacificyearlymeeting.org/view</a>';
+http://register.pacificyearlymeeting.org/view</a></p>\
+<p>If you are through entering the registrations for yourself and your family, \
+please send a check for at least 25% (100% if you are claiming the early registration discount) \
+of the total for all persons, minus expected assistance to the following address.</p>\
+<p>Berkeley Registration Team, 2151 Vine St., Berkeley, CA 94709</p>';
 
             Email.send({
                 from: from,
