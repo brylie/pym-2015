f531963c7db5835e86c956ed0b3c7e22f90463e5
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-04-27 14:15:41 +0300

    Added forgot password route.

--- both/accounts/config.js
+++ both/accounts/config.js
@@ -1,6 +1,7 @@
 AccountsTemplates.configureRoute('signIn', {layoutTemplate: 'appLayout'});
 AccountsTemplates.configureRoute('signUp', {layoutTemplate: 'appLayout'});
 AccountsTemplates.configureRoute('ensureSignedIn', {layoutTemplate: 'appLayout'});
+AccountsTemplates.configureRoute('forgotPwd', {layoutTemplate: 'appLayout'});
 AccountsTemplates.configureRoute('resetPwd', {
     layoutTemplate: 'appLayout',
     path: 'reset-password'
