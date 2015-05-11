f641f9beffe69676ac5766b66db29da288eebffb
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-05-11 16:45:08 +0300

    Edit registration set reactive variables.

--- client/templates/register/edit/edit.js
+++ client/templates/register/edit/edit.js
@@ -1,7 +1,9 @@
-Template.editRegistration.helpers({
-    // Get the current registration from the URL path
-    getRegistration: function () {
-        var registrationId = Router.current().params._id;
-        return Registrations.findOne(registrationId);
+Template.editRegistration.rendered = function () {
+    setReactiveVars();
+};
+
+Template.editRegistration.events({
+    'change form': function() {
+        setReactiveVars();
     }
 });
