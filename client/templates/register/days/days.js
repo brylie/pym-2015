7d7211fcd0ceed08b899e40353c9037cf6a81c2b
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-05-13 09:38:31 +0300

    Created days options for daily overnight.

---
+++ client/templates/register/days/days.js
@@ -0,0 +1,13 @@
+Template.days.helpers({
+  'dailyOvernightOptions': function () {
+    var options = [
+      {'label': 'Monday', 'value': 'Monday'},
+      {'label': 'Tuesday', 'value': 'Tuesday'},
+      {'label': 'Wednesday', 'value': 'Wednesday'},
+      {'label': 'Thursday', 'value': 'Thursday'},
+      {'label': 'Friday', 'value': 'Friday'},
+    ];
+
+    return options;
+  }
+});
