2b7e7466cbbab7a6ae90b45b6985a361f0b4cfb4
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-05-14 18:56:05 +0300

    Cleanup unused routes.

--- both/router/routes.js
+++ both/router/routes.js
@@ -6,11 +6,6 @@ Router.route('/dashboard', {
 
 });
 
-//Router.route('/', function () {
-//    //this.render('welcome', {data: {title: '2015 PYM Registration'}});
-//    this.render('register', {data: {title: '2015 Pacific Yearly Meeting'}});
-//});
-
 Router.route('/register');
 
 Router.route('/edit/:_id', {
@@ -21,11 +16,6 @@ Router.route('/edit/:_id', {
 Router.route('/view', {
 
 });
-
-
-//Router.route('/registration/:_id', function () {
-//    this.render('registration');
-//});
 
 var requiresUserLogin = function () {
     if (!Meteor.user()) {
