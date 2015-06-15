59878c7c6eaad2572ddae6119f26f154188b8384
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-06-15 10:53:13 +0300

    Cancel registration event and confirmation.

--- client/templates/dashboard/registrant/registrant.js
+++ client/templates/dashboard/registrant/registrant.js
@@ -1,4 +1,28 @@
 Template.dashboardRegistrant.events({
+  'click .cancelRegistration': function () {
+    // Get current registration
+    var registrationId = this._id;
+
+    // Make sure the user wants to cancel the registration
+    bootbox.dialog({
+      message: "Note: This cannot be undone.",
+      title: "Cancel this registration?",
+      buttons: {
+        cancelRegistration: {
+          label: "Yes, cancel registration.",
+          className: "btn-warning",
+          callback: function() {
+            // Call the cancel registration method for this registration ID
+            Meteor.call("cancelRegistration", registrationId);
+          }
+        },
+        doNotCancel: {
+          label: "No, do not cancel registration.",
+          className: "btn-success"
+        }
+      }
+    });
+  },
   'click .deleteRegistration': function () {
     // Make sure the user wants to delete the registration
     if (confirm("Delete this registration?")) {
