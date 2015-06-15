5fbead6708af08efb76f2631c1e3db521a041a18
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-06-15 10:57:58 +0300

    Cancel registration method.

--- server/methods/registration.js
+++ server/methods/registration.js
@@ -2,4 +2,8 @@ Meteor.methods({
 //    'calculateRegistrationCost': function (ageGroup, registrationType, accommodations, days) {
 //        return 200;
 //    }
+  "cancelRegistration": function (registrationId) {
+    // Set the registration type to "cancelled"
+    Registrants.update(registrationId, {$set: {registrationType: "cancelled"}});
+  }
 });
