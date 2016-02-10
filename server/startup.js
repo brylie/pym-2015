02485862e0377e3c706ef4025dcbf02cc8c44032
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-02-10 18:44:11 +0200

    Enable demonstration registrar

--- server/startup.js
+++ server/startup.js
@@ -1,17 +1,17 @@
 Meteor.startup(function () {
     // If testing registrar doesn't exist
-//    if (Meteor.users.find({emails: {$elemMatch: {'address': 'registrar@example.com'}}}).fetch().length === 0) {
-//        // Create a registrar user object
-//        var registrarUser = {
-//            name: "Registrar Test",
-//            email: 'registrar@example.com',
-//            password: 'testing123'
-//        };
-//
-//        // Create user in database
-//        var id = Accounts.createUser(registrarUser);
-//
-//        // Assign registrar user to registrar role
-//        Roles.addUsersToRoles(id, 'registrar');
-//    }
+   if (Meteor.users.find({emails: {$elemMatch: {'address': 'registrar@example.com'}}}).fetch().length === 0) {
+       // Create a registrar user object
+       var registrarUser = {
+           name: "Registrar Test",
+           email: 'registrar@example.com',
+           password: 'testing123'
+       };
+
+       // Create user in database
+       var id = Accounts.createUser(registrarUser);
+
+       // Assign registrar user to registrar role
+       Roles.addUsersToRoles(id, 'registrar');
+   }
 });
