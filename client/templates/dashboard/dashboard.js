286511848bbd9572106bc623b8281ffc5c7bfcf1
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-02-11 20:21:14 +0200

    Add template level subscription

--- client/templates/dashboard/dashboard.js
+++ client/templates/dashboard/dashboard.js
@@ -1,3 +1,10 @@
+Template.dashboard.onCreated = function () {
+  // Get reference to template instance
+  const instance = this;
+
+  instance.subscribe('registrants');
+}
+
 Template.dashboard.helpers({
   'registrants': function () {
     return Registrants.find();
