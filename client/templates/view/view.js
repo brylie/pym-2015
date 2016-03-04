a8f12f32d2ff7baa5d9669b7dfa0c069472d1af3
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-04 11:10:23 +0200

    Add subscription to myRegistrants

--- client/templates/view/view.js
+++ client/templates/view/view.js
@@ -1,3 +1,11 @@
+Template.registrantsView.onCreated(function () {
+  // Get reference to template instance
+  const instance = this;
+
+  // Subscribe to registrants for current user
+  instance.subscribe("myRegistrants");
+});
+
 Template.registrantsView.helpers({
     'registrants': function () {
         return Registrants.find();
