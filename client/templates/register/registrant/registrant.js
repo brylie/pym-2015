9b509856ab255e89cc1d8f5bcfdcfebf20341af9
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-04 12:24:29 +0200

    Add registrantAgeIsBelow method

--- client/templates/register/registrant/registrant.js
+++ client/templates/register/registrant/registrant.js
@@ -36,6 +36,15 @@ Template.registrantDetails.helpers({
         } else {
             return false;
         }
+    },
+    "registrantAgeIsBelow": function (ageThreshold) {
+      // Get current age value from form
+      let age = AutoForm.getFieldValue("age")
+
+      if (age) {
+        // Compare current value versus desired age threshold
+        return age < ageThreshold;
+      }
     }
 });
 
