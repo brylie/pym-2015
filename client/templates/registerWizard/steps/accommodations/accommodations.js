c4712514da56f395838647f8e39ce417b5a5bdaf
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-13 13:27:11 +0200

    Add registrationTypeOptions; cleanup

--- client/templates/registerWizard/steps/accommodations/accommodations.js
+++ client/templates/registerWizard/steps/accommodations/accommodations.js
@@ -5,7 +5,7 @@ Template.wizardAccommodations.helpers({
     * If accommodations are required, return true.
     */
     // // Get current age value from form
-    let registrationType = AutoForm.getFieldValue("registrationType")
+    let registrationType = AutoForm.getFieldValue("registrationType");
 
     // If daily or weekly, return true, otherwise return false
     if (registrationType === 'daily' || registrationType === 'weekly') {
@@ -13,5 +13,13 @@ Template.wizardAccommodations.helpers({
     } else {
         return false;
     }
+  },
+  'registrationTypeOptions': function () {
+      // registration types used on the registration form
+      return [
+          {label: "Commuter (sleeping elsewhere)", value: "commuter"},
+          {label: "Daily (staying overnight)", value: "daily"},
+          {label: "Full Week (staying overnight all nights)", value: "weekly"}
+      ];
   }
 });
