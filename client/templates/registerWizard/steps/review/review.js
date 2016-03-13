f31da2413385e0c8260b41ccf5011672dd628579
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-13 16:06:05 +0200

    Get registration data from wizard

--- client/templates/registerWizard/steps/review/review.js
+++ client/templates/registerWizard/steps/review/review.js
@@ -1,5 +1,7 @@
-Template.wizardReview.helpers({
-  'test': function () {
-    console.log("test");
-  }
+Template.wizardReview.onCreated(function () {
+  // Get reference to template instance
+  const instance = this;
+
+  // Get form data as instance variable
+  instance.registration = instance.data.wizard.mergedData();
 });
