a8f6462c551bcb0bd4af33e43327522700f56164
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-13 18:17:40 +0200

    Add subtotal helper

--- client/templates/registerWizard/steps/review/review.js
+++ client/templates/registerWizard/steps/review/review.js
@@ -74,6 +74,22 @@ Template.wizardReview.helpers({
       return discountDays * halfDayDiscountAmount;
     }
   },
+  'subtotal': function () {
+    // Get reference to template instance
+    const instance = Template.instance();
+
+    // Get registration from template instance
+    const registration = instance.registration;
+
+    // Adjust attributes of registration data to match accommodations fee function
+    registration.type = registration.registrationType;
+    registration.ageGroup = calculateAgeGroup(registration.age);
+
+    // Calculate registration fee
+    const subtotal = calculateRegistrationSubtotal(registration);
+
+    return subtotal;
+  },
   'lateFee': function () {
     // Check if current date is past registration deadline
     if (moment().utc() > moment(lateRegistrationBegins).utc()) {
