b389257606addf78f225aa4cb4961d7882038447
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-13 13:27:29 +0200

    Add accommodationsOptions

--- client/templates/registerWizard/steps/accommodations/accommodations.js
+++ client/templates/registerWizard/steps/accommodations/accommodations.js
@@ -21,5 +21,15 @@ Template.wizardAccommodations.helpers({
           {label: "Daily (staying overnight)", value: "daily"},
           {label: "Full Week (staying overnight all nights)", value: "weekly"}
       ];
+  },
+  'accommodationsOptions': function () {
+    // TODO: determine whether it is necessary to show age/age group specific options
+    return [
+      {label: "Camping", value: "camping"},
+      {label: "Dorm", value: "dorm"},
+      {label: "Semi-private", value: "semiprivate"},
+      {label: "Junior Yearly Meeting", value: "jym"},
+      {label: "Young Adult Friends", value: "yaf"}
+    ];
   }
 });
