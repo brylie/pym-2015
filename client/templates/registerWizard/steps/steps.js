6de2e41e1368fa28d8fbae2c4c8df637e1cf0691
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-13 12:10:29 +0200

    Add stepClass helper

---
+++ client/templates/registerWizard/steps/steps.js
@@ -0,0 +1,14 @@
+
+Template.wizardSteps.helpers({
+  stepClass: function(id) {
+    var activeStep = this.wizard.activeStep();
+    var step  = this.wizard.getStep(id);
+    if (activeStep && activeStep.id === id) {
+      return 'active';
+    }
+    if (step.data()) {
+      return 'completed';
+    }
+    return 'disabled';
+  }
+});
