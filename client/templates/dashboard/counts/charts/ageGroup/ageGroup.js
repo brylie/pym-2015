dc0836e81e6cc5d94e2b6a8181a3707a01ea92ae
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-14 14:16:51 +0200

    Set tick marks to integers only; comments

--- client/templates/dashboard/counts/charts/ageGroup/ageGroup.js
+++ client/templates/dashboard/counts/charts/ageGroup/ageGroup.js
@@ -41,17 +41,53 @@ Template.dashboardChartsAgeGroup.onRendered(function () {
         return ageGroupCount;
       });
 
+      // Get an array of all age group count values
+      const ageGroupCountValues = _.map(parsedAgeGroupData, function (ageGroupObject) {
+        // Get the count for current age group
+        return ageGroupObject.count;
+      });
+
+      // Determine the minimum and maximum age group count
+      const minimumAgeGroupCount = d3.min(ageGroupCountValues);
+      const maximumAgeGroupCount = d3.max(ageGroupCountValues);
+
+      // Create a tickmarks placeholder
+      let numberOfTickMarks;
+
+      // Set chart tickmark number based on difference between min and max count
+      if (maximumAgeGroupCount - minimumAgeGroupCount < 10) {
+        numberOfTickMarks = maximumAgeGroupCount - minimumAgeGroupCount;
+      } else {
+        // Chart has a maximum of ten tick marks
+        numberOfTickMarks = 10;
+      }
+
       // Get reference to template svg placeholder
       const svg = dimple.newSvg("#chartContainer", "100%", "100%");
 
       // Construct a dimple chart
       const ageGroupChart = new dimple.chart(svg, parsedAgeGroupData);
       ageGroupChart.setBounds("7%", "5%", "90%", "80%")
-      ageGroupChart.addCategoryAxis("x", "ageGroup");
+
+      // Add x and y axis values to chart
+      const xAxis = ageGroupChart.addCategoryAxis("x", "ageGroup");
       const yAxis = ageGroupChart.addMeasureAxis("y", "count");
+
+      // Adjust y axis settings
+
+      // Show only integer tick marks
       yAxis.tickFormat = "d";
+
+      // Capitalize the y axis label
       yAxis.title = "Count";
+
+      // Remove grid lines from chart
       yAxis.showGridlines = false;
+
+      // Set tick marks to value calculated above
+      yAxis.ticks = numberOfTickMarks;
+
+      // Render the bar plot
       ageGroupChart.addSeries(null, dimple.plot.bar);
       ageGroupChart.draw();
     }
