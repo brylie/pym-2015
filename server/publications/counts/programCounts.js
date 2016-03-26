b72f93016f9409f237ec20423f2bf0f04ada9d57
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-26 12:46:27 +0200

    Add YAF program registrant count

--- server/publications/counts/programCounts.js
+++ server/publications/counts/programCounts.js
@@ -7,3 +7,13 @@ Meteor.publish('jymCount', function() {
     Registrants.find({ jymProgram: "yes"})
   );
 });
+
+/*
+Count the number or Young Adult Friends registrants.
+*/
+Meteor.publish('yafCount', function() {
+  Counts.publish(this,
+    'yafCount',
+    Registrants.find({ yafProgram: "yes"})
+  );
+});
