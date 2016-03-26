5a938d6b8330d92bd6a4e4bbeaba4bba1a8c394b
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-26 10:41:28 +0200

    Add registrantAgeIsBetween helper; cleanup

--- client/templates/registerWizard/steps/registrant/registrant.js
+++ client/templates/registerWizard/steps/registrant/registrant.js
@@ -1,11 +1,24 @@
 Template.wizardRegistrant.helpers({
   "registrantAgeIsBelow": function (ageThreshold) {
-    // // Get current age value from form
+    // Get current age value from form
     let age = AutoForm.getFieldValue("age")
 
     if (age) {
       // Compare current value versus desired age threshold
       return age < ageThreshold;
+    }
+  },
+  "registrantAgeIsBetween": function (lowerAge, upperAge) {
+    /*
+    Check if registrant age is within lower age and upper age limits (inclusive)
+    */
+
+    // Get current age value from form
+    let age = AutoForm.getFieldValue("age")
+
+    if (age) {
+      // Compare current value versus desired age range
+      return (lowerAge <= age) && (age <= upperAge);
     }
   }
 });
