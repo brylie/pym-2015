410c808f8c778e8b4629863a0680a54dcd92c049
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-03-26 12:46:56 +0200

    Add jymCount and yafCount helpers; cleanup

--- client/templates/dashboard/counts/counts.js
+++ client/templates/dashboard/counts/counts.js
@@ -1,10 +1,20 @@
 Template.dashboardCounts.created = function () {
     this.subscribe("linensCount");
+    this.subscribe("jymCount");
+    this.subscribe("yafCount");
 };
 
 Template.dashboardCounts.helpers({
     'linensCount': function () {
         // Return the linens count
-        return linensCount = Counts.get('linensCount');
+        return Counts.get('linensCount');
+    },
+    'jymCount': function () {
+        // Return the linens count
+        return Counts.get('jymCount');
+    },
+    'yafCount': function () {
+        // Return the linens count
+        return Counts.get('yafCount');
     }
 });
