80c7bdc34d8cbc790f9a7e103bc34923268616c8
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-05-01 13:07:33 +0300

    Simplify accommodations options

--- client/templates/edit/accommodations/accommodations.js
+++ client/templates/edit/accommodations/accommodations.js
@@ -1,7 +1,3 @@
-Template.accommodations.rendered = function () {
-
-}
-
 Template.accommodations.helpers({
     'requiresAccommodations': function () {
         /*
@@ -20,46 +16,11 @@ Template.accommodations.helpers({
         }
     },
     'accommodationsOptions': function () {
-        /*
-        * Update the accommodations options
-        * based on the age group of the registrant.
-        */
-
-        // get the age group from reactive variables
-        var ageGroup = ageGroupVar.get();
-
-        // return accommodation options based on age group
-        switch (ageGroup) {
-            case 'child':
-                return [
-                    {label: "Camping", value: "camping"},
-                    {label: "Dorm", value: "dorm"},
-                    {label: "Semi-private", value: "semiprivate"}
-                ];
-            case 'youth':
-                return [
-                    {label: "Camping", value: "camping"},
-                    {label: "Dorm", value: "dorm"},
-                    {label: "Semi-private", value: "semiprivate"}
-                ];
-            case 'teen':
-                return [
-                    {label: "Camping", value: "camping"},
-                    {label: "Dorm", value: "dorm"},
-                    {label: "Semi-private", value: "semiprivate"}
-                ];
-            case 'youngAdult':
-                return [
-                    {label: "Camping", value: "camping"},
-                    {label: "Dorm", value: "dorm"},
-                    {label: "Semi-private", value: "semiprivate"}
-                ];
-            case 'adult':
-                return [
-                    {label: "Camping", value: "camping"},
-                    {label: "Dorm", value: "dorm"},
-                    {label: "Semi-private", value: "semiprivate"}
-                ];
-        }
+      // Return an array of accommodations options objects
+      return [
+          {label: "Camping", value: "camping"},
+          {label: "Dorm", value: "dorm"},
+          {label: "Semi-private", value: "semiprivate"}
+      ];
     }
 });
