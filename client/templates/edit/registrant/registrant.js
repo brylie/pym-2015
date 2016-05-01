1fb06997526bc65e2b592266d247ac7115894107
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-05-01 13:08:11 +0300

    Simplify days checkbox reset

--- client/templates/edit/registrant/registrant.js
+++ client/templates/edit/registrant/registrant.js
@@ -46,14 +46,10 @@ Template.registrantDetails.events({
     'change #registration_type': function () {
         // clean up form values for accommodations and days
         // to make sure no erroneous values remain
-
         resetReactiveVars();
 
         // Clear all day checkboxes
-        $('input[name="days"]').each(function() {
-            // make sure day is not checked
-            this.checked = false;
-        });
+        $('input[name="days"]').prop('checked', false);
     },
     'change #age': function () {
         // Get the value of the age field
