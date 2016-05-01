03be49d36042ecf2ad47584b67b6f061d9ce4b05
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-05-01 11:30:43 +0300

    Add template level subscription

--- client/templates/register/edit/edit.js
+++ client/templates/register/edit/edit.js
@@ -1,3 +1,17 @@
+Template.editRegistration.created = function () {
+  // Get reference to template instance
+  var instance = this;
+
+  // Get reference to router
+  var router = Router.current();
+
+  // Get registrant ID from router parameters
+  var registrantId = router.params._id;
+
+  // Subscribe to current registrant
+  instance.subscribe("singleRegistrant", registrantId);
+};
+
 Template.editRegistration.rendered = function () {
     setReactiveVars();
 };
