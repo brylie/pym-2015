355f43d157254546f6892bba81c7ef73eb3bbc14
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-05-01 13:12:10 +0300

    Consistent whitespace

--- client/templates/edit/registrant/registrant.js
+++ client/templates/edit/registrant/registrant.js
@@ -1,87 +1,87 @@
 Template.registrantDetails.helpers({
-    'registrationTypeOptions': function () {
-        // registration types used on the registration form
-        return [
-            {label: "Commuter (sleeping elsewhere)", value: "commuter"},
-            {label: "Daily (staying overnight)", value: "daily"},
-            {label: "Full Week (staying overnight all nights)", value: "weekly"}
-        ];
-    },
-    "registrantAgeIsBelow": function (ageThreshold) {
-      // Get current age value from form
-      let age = AutoForm.getFieldValue("age")
+  'registrationTypeOptions': function () {
+    // registration types used on the registration form
+    return [
+      {label: "Commuter (sleeping elsewhere)", value: "commuter"},
+      {label: "Daily (staying overnight)", value: "daily"},
+      {label: "Full Week (staying overnight all nights)", value: "weekly"}
+    ];
+  },
+  "registrantAgeIsBelow": function (ageThreshold) {
+    // Get current age value from form
+    let age = AutoForm.getFieldValue("age")
 
-      if (age) {
-        // Compare current value versus desired age threshold
-        return age < ageThreshold;
-      }
-    },
-    "registrantAgeIs": function (age) {
-      // Get current age value from form
-      let registrantAge = AutoForm.getFieldValue("age")
-
-      if (age) {
-        // Compare current value versus desired age threshold
-        return registrantAge === age;
-      }
-    },
-    "registrantAgeIsEqualToOrAbove": function (ageThreshold) {
-      // Get current age value from form
-      let age = AutoForm.getFieldValue("age")
-
-      if (age) {
-        // Compare current value versus desired age threshold
-        return age >= ageThreshold;
-      }
-    },
-    "yesNoOptions": function () {
-      return [
-        {label: "Yes", value: "yes"},
-        {label: "No", value: "no"}
-      ];
+    if (age) {
+      // Compare current value versus desired age threshold
+      return age < ageThreshold;
     }
+  },
+  "registrantAgeIs": function (age) {
+    // Get current age value from form
+    let registrantAge = AutoForm.getFieldValue("age")
+
+    if (age) {
+      // Compare current value versus desired age threshold
+      return registrantAge === age;
+    }
+  },
+  "registrantAgeIsEqualToOrAbove": function (ageThreshold) {
+    // Get current age value from form
+    let age = AutoForm.getFieldValue("age")
+
+    if (age) {
+      // Compare current value versus desired age threshold
+      return age >= ageThreshold;
+    }
+  },
+  "yesNoOptions": function () {
+    return [
+      {label: "Yes", value: "yes"},
+      {label: "No", value: "no"}
+    ];
+  }
 });
 
 Template.registrantDetails.events({
-    'change #registration_type': function () {
-        // clean up form values for accommodations and days
-        // to make sure no erroneous values remain
-        resetReactiveVars();
+  'change #registration_type': function () {
+    // clean up form values for accommodations and days
+    // to make sure no erroneous values remain
+    resetReactiveVars();
 
-        // Clear all day checkboxes
-        $('input[name="days"]').prop('checked', false);
-    },
-    'change #age': function () {
-        // Get the value of the age field
-        var ageValue = $('#age').val();
+    // Clear all day checkboxes
+    $('input[name="days"]').prop('checked', false);
+  },
+  'change #age': function () {
+    // Get the value of the age field
+    var ageValue = $('#age').val();
 
-        // Calculate the age group based on the age value
-        var ageGroup = calculateAgeGroup(ageValue);
+    // Calculate the age group based on the age value
+    var ageGroup = calculateAgeGroup(ageValue);
 
-        // Set the age group reactive variable
-        // for price calculations
-        ageGroupVar.set(ageGroup);
-    },
-    "change #children-program": function (event, instance) {
-      const childrenProgramSelection = event.target.value;
+    // Set the age group reactive variable
+    // for price calculations
+    ageGroupVar.set(ageGroup);
+  },
+  "change #children-program": function (event, instance) {
+    const childrenProgramSelection = event.target.value;
 
-      if (childrenProgramSelection === "yes") {
-        // Set JYM program value to "No"
-        $("[name=jymProgram]").val(["no"]);
-      } else if (childrenProgramSelection === "no") {
-        // Set JYM program value to "Yes"
-        $("[name=jymProgram]").val(["yes"]);
-      }
-    },
-    "change #jym-program": function (event, instance) {
-      const jymProgramSelection = event.target.value;
-
-      if (jymProgramSelection === "yes") {
-        // Set Children's program value to "No"
-        $("[name=childrenProgram]").val(["no"]);;
-      } else if (jymProgramSelection === "no") {
-        // Set Children's program value to "Yes"
-        $("[name=childrenProgram]").val(["yes"]);
-      }
+    if (childrenProgramSelection === "yes") {
+      // Set JYM program value to "No"
+      $("[name=jymProgram]").val(["no"]);
+    } else if (childrenProgramSelection === "no") {
+      // Set JYM program value to "Yes"
+      $("[name=jymProgram]").val(["yes"]);
     }
+  },
+  "change #jym-program": function (event, instance) {
+    const jymProgramSelection = event.target.value;
+
+    if (jymProgramSelection === "yes") {
+      // Set Children's program value to "No"
+      $("[name=childrenProgram]").val(["no"]);;
+    } else if (jymProgramSelection === "no") {
+      // Set Children's program value to "Yes"
+      $("[name=childrenProgram]").val(["yes"]);
+    }
+  }
 });
